47af98f7993106c4d37a6e460ee8676dbacbe661
NashvillePublicLibrary <james.staub@nashville.gov>
2018-06-17 11:27:15 -0500

    Creates patrons with Carl.X Patron API

--- NashvilleCarlXPatronLoader.php
+++ NashvilleCarlXPatronLoader.php
@@ -1,6 +1,14 @@
 <?php
 
 // echo 'SYNTAX: path/to/php NashvilleCarlXPatronLoader.php, e.g., $ sudo /opt/rh/php55/root/usr/bin/php NashvilleCarlXPatronLoader.php\n';
+// 
+// TO DO: retry after oracle connect error
+// TO DO: test whether setting PIN works... appears to set as 9999
+// TO DO: UDF
+// TO DO: CREATE GUARANTOR NOTE
+	// $request->Patron->Notes					= $patron[25]; // Patron Notes
+// TO DO: STUDENT IMAGES
+// TO DO: STAFF
 
 date_default_timezone_set('America/Chicago');
 $startTime = microtime(true);
@@ -27,14 +35,11 @@ $sql = fread($get_patrons_mnps_carlx_filehandle, filesize("get_patrons_mnps_carl
 fclose($get_patrons_mnps_carlx_filehandle);
 
 $stid = oci_parse($conn, $sql);
-// consider tuning oci_set_prefetch to improve performance
-// https://docs.oracle.com/database/121/TDPPH/ch_eight_query.htm#TDPPH172
+// consider tuning oci_set_prefetch to improve performance. See https://docs.oracle.com/database/121/TDPPH/ch_eight_query.htm#TDPPH172
 oci_set_prefetch($stid, 10000);
 oci_execute($stid);
 // start a new file for the CarlX patron extract
-$patrons_mnps_carlx_filehandle;
 $patrons_mnps_carlx_filehandle = fopen($reportPath . "patrons_mnps_carlx.csv", 'w');
-        
 while (($row = oci_fetch_array ($stid, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
 	// CSV OUTPUT
 	fputcsv($patrons_mnps_carlx_filehandle, $row);
@@ -44,131 +49,102 @@ echo "Patrons MNPS CARLX retrieved and written\n";
 oci_free_statement($stid);
 oci_close($conn);
 
-// TO DO: handle staff
-
-$records = array();
-// $icfile = "../data/INFINITECAMPUS_STUDENT.csv";
-$icfile = "../data/TEST-INFINITECAMPUS_STUDENT.csv";
-
-// TO DO: Diff ad hoc csv result against ic extract
-// TO DO: ic extract should quote like php csv output
-// TO DO: create new patrons
-
 // Using shell instead of php as per https://stackoverflow.com/questions/35999597/importing-csv-file-into-sqlite3-from-php#36001304
 // FROM https://www.sqlite.org/cli.html:
 // "The dot-commands are interpreted by the sqlite3.exe command-line program, not by SQLite itself."
 // "So none of the dot-commands will work as an argument to SQLite interfaces like sqlite3_prepare() or sqlite3_exec()."
 
-exec("sqlite3 ../data/ic2carlx.db < NashvilleCarlXPatronLoaderImport.sql");
+exec("bash format_patrons_mnps_infinitecampus.sh");
+exec("sqlite3 ../data/ic2carlx.db < patrons_mnps_compare.sql");
 
-$db = new SQLite3('../data/ic2carlx.db');
-
-if(!$db) {
-      echo $db->lastErrorMsg();
-} else {
-//      echo "Opened database successfully\n";
+$all_rows = array();
+$fhnd = fopen("../data/patrons_mnps_carlx_add.csv", "r");
+if ($fhnd){
+	$header = fgetcsv($fhnd);
+	while ($row = fgetcsv($fhnd)) {
+		$all_rows[] = array_combine($header, $row);
+	}
 }
+//print_r($all_rows);
 
-$sql = <<<EOT
-	select *
-	from infinitecampus
-	left join carlx on infinitecampus.PatronID = carlx.PatronID
-	where carlx.PatronID IS NULL
-	order by infinitecampus.PatronID
-	;
-EOT;
-
-$aCreatePatrons = $db->query($sql);
-while ($patron = $aCreatePatrons->fetchArray(2)) {
-	if ($patron[0] == 'Patron ID') {continue;}
-	var_dump($patron[0]);
-
-// UPDATE OR CREATE PATRON
+foreach ($all_rows as $patron) {
 	// TESTING
-	if ($patron[0] > 190999200) { exit(); }
-	// CREATE PATRON UPDATE REQUEST
+	if ($patron['PatronID'] > 190999120) { exit(); }
+	// CREATE REQUEST
 	$request							= new stdClass();
 	$request->Modifiers						= '';
-	$request->SearchType						= 'Patron ID';
-	$request->SearchID						= $patron[0]; // Patron ID
 	$request->Patron						= new stdClass();
-	// $request->Patron->PatronID					= $patron[0]; // Patron ID
-	$request->Patron->PatronType					= $patron[1]; // Patron Type
-	$request->Patron->LastName					= $patron[2]; // Patron Name Last
-	$request->Patron->FirstName					= $patron[3]; // Patron Name First
-	$request->Patron->MiddleName					= $patron[4]; // Patron Name Middle
-	$request->Patron->SuffixName					= $patron[5]; // Patron Name Suffix
+	$request->Patron->PatronID					= $patron['PatronID']; // Patron ID
+	$request->Patron->PatronType					= $patron['Borrowertypecode']; // Patron Type
+	$request->Patron->LastName					= $patron['Patronlastname']; // Patron Name Last
+	$request->Patron->FirstName					= $patron['Patronfirstname']; // Patron Name First
+	$request->Patron->MiddleName					= $patron['Patronmiddlename']; // Patron Name Middle
+	$request->Patron->SuffixName					= $patron['Patronsuffix']; // Patron Name Suffix
 	$request->Patron->Addresses					= new stdClass();
 	$request->Patron->Addresses->Address[0]				= new stdClass();
 	$request->Patron->Addresses->Address[0]->Type			= 'Primary';
-	$request->Patron->Addresses->Address[0]->Street			= $patron[6]; // Patron Address Street
-	$request->Patron->Addresses->Address[0]->City			= $patron[7]; // Patron Address City
-	$request->Patron->Addresses->Address[0]->State			= $patron[8]; // Patron Address State
-	$request->Patron->Addresses->Address[0]->PostalCode		= $patron[9]; // Patron Address ZIP Code
-	// $request->Patron->Addresses->Address[2]->Street		= $patron[10]; // Patron Secondary Address Street
-	// $request->Patron->Addresses->Address[2]->City		= $patron[11]; // Patron Secondary Address City
-	// $request->Patron->Addresses->Address[2]->State		= $patron[12]; // Patron Secondary Address State
-	// $request->Patron->Addresses->Address[2]->PostalCode		= $patron[13]; // Patron Secondary Address ZIP Code
-	$request->Patron->Phone1					= $patron[14]; // Patron Primary Phone
-	// $request->Patron->Phone2					= $patron[15]; // Patron Secondary Phone
-	// $request->Patron->AltId					= $patron[16]; // Patron Alternate ID
-	// $request->Patron->Non-Validated Stat Code			= $patron[17]; // Patron Non-Validated Stat Code
-	$request->Patron->DefaultBranch					= $patron[18]; // Patron Default Branch
-	// $request->Patron->Validated Stat Code			= $patron[19]; // Patron Validated Stat Code
-	// MIGHT need logic to properly process changes in PatronStatusCode
-	// $request->Patron->PatronStatusCode				= $patron[20]; // Patron Status Code
-	// $request->Patron->RegistrationDate				= $patron[21]; // Patron Registrtation Date
-	// $request->Patron->LastActionDate				= $patron[22]; // Patron Last Action Date
-	$request->Patron->ExpirationDate				= date_create_from_format('Y-m-d',$patron[23])->format('c'); // Patron Expiration Date as ISO 8601
-	$request->Patron->Email						= $patron[24]; // Patron Email
-	// $request->Patron->Notes					= $patron[25]; // Patron Notes
-	$request->Patron->BirthDate					= $patron[26]; // Patron Birth Date as Y-m-d
+	$request->Patron->Addresses->Address[0]->Street			= $patron['PrimaryStreetAddress']; // Patron Address Street
+	$request->Patron->Addresses->Address[0]->City			= $patron['PrimaryCity']; // Patron Address City
+	$request->Patron->Addresses->Address[0]->State			= $patron['PrimaryState']; // Patron Address State
+	$request->Patron->Addresses->Address[0]->PostalCode		= $patron['PrimaryZipCode']; // Patron Address ZIP Code
+	// $request->Patron->Phone1					= $patron['PrimaryPhoneNumber']; // Patron Primary Phone
+	$request->Patron->Phone2					= $patron['SecondaryPhoneNumber']; // Patron Secondary Phone
+	$request->Patron->DefaultBranch					= $patron['DefaultBranch']; // Patron Default Branch
+	$request->Patron->LastActionBranch				= $patron['DefaultBranch']; // Patron Last Action Branch
+	$request->Patron->LastEditBranch				= $patron['DefaultBranch']; // Patron Last Edit Branch
+	$request->Patron->RegBranch					= $patron['DefaultBranch']; // Patron Registration Branch
+	$request->Patron->Email						= $patron['EmailAddress']; // Patron Email
+	$request->Patron->BirthDate					= $patron['BirthDate']; // Patron Birth Date as Y-m-d
 	// Sponsor: Homeroom Teacher
 	$request->Patron->Addresses->Address[1]				= new stdClass();
 	$request->Patron->Addresses->Address[1]->Type			= 'Secondary';
-	$request->Patron->Addresses->Address[1]->Street			= $patron[32]; // Patron Homeroom Teacher ID
-	$request->Patron->SponsorName					= $patron[33];
+	$request->Patron->Addresses->Address[1]->Street			= $patron['TeacherID']; // Patron Homeroom Teacher ID
+	$request->Patron->SponsorName					= $patron['TeacherName'];
+	if (stripos($patron['PatronID'],'190999') == 0) {
+		$request->Patron->PatronPIN				= '7357';
+	} else {
+		$request->Patron->PatronPIN				= substr($patron['BirthDate'],5,2) . substr($patron['BirthDate'],8,2);
+	}
 	// NON-CSV STUFF
-	$request->Patron->LastEditBranch				= $patron[18]; // Patron Default Branch
+	$request->Patron->EmailNotices					= 'send email';
+	$request->Patron->ExpirationDate				= date_create_from_format('Y-m-d',$patron['ExpirationDate'])->format('c'); // Patron Expiration Date as ISO 8601
+	$request->Patron->LastActionDate				= date('c'); // Last Action Date, format ISO 8601
 	$request->Patron->LastEditDate					= date('c'); // Patron Last Edit Date, format ISO 8601
 	$request->Patron->LastEditedBy					= 'PIK'; // Pika Patron Loader
-// TO DO: accommodate patron manually updates
-	// $request->Patron->EmailNotices				= 'send email';
-	// $request->Patron->LastActionDate				= date('c'); // Last Action Date, format ISO 8601
-// TO DO: accommodate patron manually updates
-	$request->Patron->PatronPIN					= substr($patron[26],5,2) . substr($patron[26],8,2);
-	// TEST PATRON PIN = 7357
-	if (stripos($patron[0],'190999') == 0) {
-		$request->Patron->PatronPIN				= '7357';
-	}
+	$request->Patron->PatronStatusCode				= 'G'; // Patron Status Code = GOOD
 	$request->Patron->PreferredAddress				= 'Sponsor';
-
+	$request->Patron->RegisteredBy					= 'PIK'; // Registered By : Pika Patron Loader
+	$request->Patron->RegistrationDate				= date('c'); // Registration Date, format ISO 8601
 //var_dump($request);
-
-//	try {
-//		$client = new SOAPClient($patronApiWsdl, array('features' => SOAP_WAIT_ONE_WAY_CALLS, 'trace' => 1));
-//		$result = $client->updatePatron($request);
-//		$result = $client->__getLastResponse();
-//var_dump($result);
-
-// CREATE PATRON
-//		if ($result && stripos($result,'ShortMessage>No matching records found') !== false) {
-			$request->Patron->RegBranch		= $patron[18]; // Patron Registration Branch
-			$request->Patron->RegisteredBy		= 'PIK'; // Registered By : Pika Patron Loader
-			$request->Patron->RegistrationDate	= date('c'); // Registration Date, format ISO 8601
-			$request->Patron->PatronStatusCode	= 'G'; // Patron Status Code = GOOD
-			try {
-$client = new SOAPClient($patronApiWsdl, array('features' => SOAP_WAIT_ONE_WAY_CALLS, 'trace' => 1));
-				$result = $client->createPatron($request);
-				$result = $client->__getLastResponse();
+	try {
+		$client = new SOAPClient($patronApiWsdl, array('features' => SOAP_WAIT_ONE_WAY_CALLS, 'trace' => 1));
+		$result = $client->createPatron($request);
+		$result = $client->__getLastResponse();
 var_dump($result);
-			} catch (Exception $e) {
-				echo $e->getMessage();
-			}
-//		} 
-//	} catch (Exception $e) {
-//		echo $e->getMessage();
-//	}
+	} catch (Exception $e) {
+		echo $e->getMessage();
+	}
+}
+
+exit();
+?>
+
+// TO DO: Update Patron User Defined Fields [Permissions]
+// Retrieve current User Defined Fields to determine which need to be created and which updated
+	$request							= new stdClass();
+	$request->Modifiers						= '';
+	$request->Patronid						= $patron[0]; // Patron ID
+	$request->Occur							= 0;
+	try {
+		$client = new SOAPClient($patronApiWsdl, array('features' => SOAP_WAIT_ONE_WAY_CALLS, 'trace' => 1));
+		$result = $client->GetPatronUserDefinedFields($request);
+		$result = $client->__getLastResponse();
+//var_dump($result);
+	} catch (Exception $e) {
+		echo $e->getMessage();
+	}
+
+
 
 /*
 // UPDATE PATRON IMAGE
@@ -197,50 +173,6 @@ var_dump($result);
 	}
 */
 
-/*
-// TO DO: Patron->UDFs ain't writing to db 20171211
-// TLC Marisa Wood wrote the class in in updatePatronRequest to provide backward compatibility in the future
-	$request->Patron->UserDefinedFields				= new stdClass();
-	// User Defined Field: Racial or Ethnic Category
-	$request->Patron->UserDefinedFields->UserDefinedField[0]	= new stdClass();
-	$request->Patron->UserDefinedFields->UserDefinedField[0]->Field	= '2';
-	$request->Patron->UserDefinedFields->UserDefinedField[0]->Value	= $patron[28]; // UDF2:Racial or Ethnic Category
-	// User Defined Field: Lap Top Check Out
-	$request->Patron->UserDefinedFields->UserDefinedField[1]	= new stdClass();
-	$request->Patron->UserDefinedFields->UserDefinedField[1]->Field	= '3';
-	$request->Patron->UserDefinedFields->UserDefinedField[1]->Value	= $patron[29]; // UDF3:Lap Top Check Out
-	// User Defined Field: Limitless Library Use
-	$request->Patron->UserDefinedFields->UserDefinedField[2]	= new stdClass();
-	$request->Patron->UserDefinedFields->UserDefinedField[2]->Field	= '4';
-	$request->Patron->UserDefinedFields->UserDefinedField[2]->Value	= $patron[30]; // UDF4:Limitless Library Use
-	// User Defined Field: Tech Opt Out
-	$request->Patron->UserDefinedFields->UserDefinedField[3]	= new stdClass();
-	$request->Patron->UserDefinedFields->UserDefinedField[3]->Field	= '1';
-	$request->Patron->UserDefinedFields->UserDefinedField[3]->Value	= $patron[31]; // UDF1:Tech Opt Out						
-*/
-
-// TO DO: Update Patron User Defined Fields [Permissions]
-// Retrieve current User Defined Fields to determine which need to be created and which updated
-	$request							= new stdClass();
-	$request->Modifiers						= '';
-	$request->Patronid						= $patron[0]; // Patron ID
-	$request->Occur							= 0;
-	try {
-		$client = new SOAPClient($patronApiWsdl, array('features' => SOAP_WAIT_ONE_WAY_CALLS, 'trace' => 1));
-		$result = $client->GetPatronUserDefinedFields($request);
-		$result = $client->__getLastResponse();
-//var_dump($result);
-	} catch (Exception $e) {
-		echo $e->getMessage();
-	}
-
-/*
-// TO DO: Compare GetPatronUserDefinedFields against incoming data 
-		$patron[28]; // UDF2:Racial or Ethnic Category [THIS SCRIPT WILL NOT ALTER VALUES]
-		$patron[29]; // UDF3:Lap Top Check Out
-		$patron[30]; // UDF4:Limitless Library Use
-		$patron[31]; // UDF1:Tech Opt Out
-*/
 
 /*
 // TO DO: Guardian notes
