0f1a28f6a2ff92bbb0bbc41529529e4dda7d42d0
James Staub <james.staub@nashville.gov>
2020-11-09 21:21:06 -0600

    Updates mnps staff put
    Removes PEAR
    Spacing

--- ic2carlx_mnps_staff_put_carlx.php
+++ ic2carlx_mnps_staff_put_carlx.php
@@ -12,14 +12,13 @@
 date_default_timezone_set('America/Chicago');
 $startTime = microtime(true);
 
-require_once 'PEAR.php';
 require_once 'ic2carlx_put_carlx.php';
 
-$configArray		= parse_ini_file('../config.pwd.ini', true, INI_SCANNER_RAW);
-$patronApiWsdl		= $configArray['Catalog']['patronApiWsdl'];
-$patronApiDebugMode	= $configArray['Catalog']['patronApiDebugMode'];
+$configArray			= parse_ini_file('../config.pwd.ini', true, INI_SCANNER_RAW);
+$patronApiWsdl			= $configArray['Catalog']['patronApiWsdl'];
+$patronApiDebugMode		= $configArray['Catalog']['patronApiDebugMode'];
 $patronApiReportMode	= $configArray['Catalog']['patronApiReportMode'];
-$reportPath		= '../data/';
+$reportPath				= '../data/';
 
 //////////////////// REMOVE CARLX PATRONS ////////////////////
 // See https://trello.com/c/lK7HgZgX for spec
@@ -38,19 +37,19 @@ foreach ($all_rows as $patron) {
 	// TESTING
 	//if ($patron['patronid'] > 999115) { break; }
 	// CREATE REQUEST
-	$requestName							= 'updatePatron';
+	$requestName						= 'updatePatron';
 	$tag								= $patron['patronid'] . ' : removePatron';
 	$request							= new stdClass();
-	$request->Modifiers						= new stdClass();
-	$request->Modifiers->DebugMode					= $patronApiDebugMode;
-	$request->Modifiers->ReportMode					= $patronApiReportMode;
-	$request->SearchType						= 'Patron ID';
-	$request->SearchID						= $patron['patronid']; // Patron ID
-	$request->Patron						= new stdClass();
-	$request->Patron->PatronType					= '38'; // Patron Type = Expired MNPS
-	$request->Patron->DefaultBranch					= 'XMNPS'; // Patron Default Branch
-	$request->Patron->LastEditBranch				= 'XMNPS'; // Patron Last Edit Branch
-	$request->Patron->RegBranch					= 'XMNPS'; // Patron Registration Branch
+	$request->Modifiers					= new stdClass();
+	$request->Modifiers->DebugMode		= $patronApiDebugMode;
+	$request->Modifiers->ReportMode		= $patronApiReportMode;
+	$request->SearchType				= 'Patron ID';
+	$request->SearchID					= $patron['patronid']; // Patron ID
+	$request->Patron					= new stdClass();
+	$request->Patron->PatronType		= '38'; // Patron Type = Expired MNPS
+	$request->Patron->DefaultBranch		= 'XMNPS'; // Patron Default Branch
+	$request->Patron->LastEditBranch	= 'XMNPS'; // Patron Last Edit Branch
+	$request->Patron->RegBranch			= 'XMNPS'; // Patron Registration Branch
 	if ($patron['collectionstatus']==0 || $patron['collectionstatus']==1 || $patron['collectionstatus']==78) {
 		$request->Patron->CollectionStatus			= 'not sent';
 	}
@@ -59,33 +58,33 @@ foreach ($all_rows as $patron) {
 //	}
 	// NON-CSV STUFF
 	if (!empty($patron['patron_seen'])) {
-		$request->Patron->ExpirationDate			= date_create_from_format('Y-m-d',$patron['patron_seen'])->format('c'); // Patron Expiration Date as ISO 8601
+		$request->Patron->ExpirationDate	= date_create_from_format('Y-m-d',$patron['patron_seen'])->format('c'); // Patron Expiration Date as ISO 8601
 	} else {
-		$request->Patron->ExpirationDate			= date('c', strtotime('yesterday')); // Patron Expiration Date as ISO 8601
+		$request->Patron->ExpirationDate	= date('c', strtotime('yesterday')); // Patron Expiration Date as ISO 8601
 	}
-	$request->Patron->LastEditDate					= date('c'); // Patron Last Edit Date, format ISO 8601
-	$request->Patron->LastEditedBy					= 'PIK'; // Pika Patron Loader
-	$request->Patron->PreferredAddress				= 'Primary';
+	$request->Patron->LastEditDate			= date('c'); // Patron Last Edit Date, format ISO 8601
+	$request->Patron->LastEditedBy			= 'PIK'; // Pika Patron Loader
+	$request->Patron->PreferredAddress		= 'Primary';
 	$result = callAPI($patronApiWsdl, $requestName, $request, $tag);
 
 // CREATE URGENT 'Former MNPS Patron' NOTE
 	// CREATE REQUEST
-	$requestName							= 'addPatronNote';
-	$tag								= $patron['patronid'] . ' : addPatronRemoveNote';
-	$request							= new stdClass();
-	$request->Modifiers						= new stdClass();
-	$request->Modifiers->DebugMode					= $patronApiDebugMode;
-	$request->Modifiers->ReportMode					= $patronApiReportMode;
-	$request->Modifiers->StaffID					= 'PIK'; // Pika Patron Loader
-	$request->Note							= new stdClass();
-	$request->Note->PatronID					= $patron['patronid']; // Patron ID
-	$request->Note->NoteType					= '800'; 
+	$requestName					= 'addPatronNote';
+	$tag							= $patron['patronid'] . ' : addPatronRemoveNote';
+	$request						= new stdClass();
+	$request->Modifiers				= new stdClass();
+	$request->Modifiers->DebugMode	= $patronApiDebugMode;
+	$request->Modifiers->ReportMode	= $patronApiReportMode;
+	$request->Modifiers->StaffID	= 'PIK'; // Pika Patron Loader
+	$request->Note					= new stdClass();
+	$request->Note->PatronID		= $patron['patronid']; // Patron ID
+	$request->Note->NoteType		= '800';
 	if (!empty($patron['patron_seen'])) {
-		$PatronExpirationDate					= $patron['patron_seen']; // Patron Expiration Date as ISO 8601
+		$PatronExpirationDate		= $patron['patron_seen']; // Patron Expiration Date as ISO 8601
 	} else {
-		$PatronExpirationDate					= date('Y-m-d', strtotime('yesterday')); // Patron Expiration Date
+		$PatronExpirationDate		= date('Y-m-d', strtotime('yesterday')); // Patron Expiration Date
 	}
-	$request->Note->NoteText					= 'Former MNPS staffer, patron expired ' . $PatronExpirationDate . '. Previous branchcode: ' . $patron['defaultbranch'] . '. Previous bty: ' . $patron['borrowertypecode'] . '. DO NOT convert this account to NPL; create new account after outstanding transactions are resolved.';
+	$request->Note->NoteText		= 'Former MNPS staffer, patron expired ' . $PatronExpirationDate . '. Previous branchcode: ' . $patron['defaultbranch'] . '. Previous bty: ' . $patron['borrowertypecode'] . '. DO NOT convert this account to NPL; create new account after outstanding transactions are resolved.';
 	$result = callAPI($patronApiWsdl, $requestName, $request, $tag);
 }
 
@@ -105,55 +104,55 @@ foreach ($all_rows as $patron) {
 	// TESTING
 	//if ($patron['patronid'] > 999115) { break; }
 	// CREATE REQUEST
-	$requestName							= 'createPatron';
+	$requestName						= 'createPatron';
 	$tag								= $patron['patronid'] . ' : ' . $requestName;
 	$request							= new stdClass();
-	$request->Modifiers						= new stdClass();
-	$request->Modifiers->DebugMode					= $patronApiDebugMode;
-	$request->Modifiers->ReportMode					= $patronApiReportMode;
-	$request->Patron						= new stdClass();
-	$request->Patron->PatronID					= $patron['patronid']; // Patron ID
-	$request->Patron->PatronType					= $patron['borrowertypecode']; // Patron Type
-	$request->Patron->LastName					= $patron['patronlastname']; // Patron Name Last
-	$request->Patron->FirstName					= $patron['patronfirstname']; // Patron Name First
-	$request->Patron->MiddleName					= $patron['patronmiddlename']; // Patron Name Middle
-	$request->Patron->SuffixName					= $patron['patronsuffix']; // Patron Name Suffix
-	$request->Patron->DefaultBranch					= $patron['defaultbranch']; // Patron Default Branch
-	$request->Patron->LastActionBranch				= $patron['defaultbranch']; // Patron Last Action Branch
-	$request->Patron->LastEditBranch				= $patron['defaultbranch']; // Patron Last Edit Branch
-	$request->Patron->RegBranch					= $patron['defaultbranch']; // Patron Registration Branch
-	$request->Patron->Email						= $patron['emailaddress']; // Patron Email
+	$request->Modifiers					= new stdClass();
+	$request->Modifiers->DebugMode		= $patronApiDebugMode;
+	$request->Modifiers->ReportMode		= $patronApiReportMode;
+	$request->Patron					= new stdClass();
+	$request->Patron->PatronID			= $patron['patronid']; // Patron ID
+	$request->Patron->PatronType		= $patron['borrowertypecode']; // Patron Type
+	$request->Patron->LastName			= $patron['patronlastname']; // Patron Name Last
+	$request->Patron->FirstName			= $patron['patronfirstname']; // Patron Name First
+	$request->Patron->MiddleName		= $patron['patronmiddlename']; // Patron Name Middle
+	$request->Patron->SuffixName		= $patron['patronsuffix']; // Patron Name Suffix
+	$request->Patron->DefaultBranch		= $patron['defaultbranch']; // Patron Default Branch
+	$request->Patron->LastActionBranch	= $patron['defaultbranch']; // Patron Last Action Branch
+	$request->Patron->LastEditBranch	= $patron['defaultbranch']; // Patron Last Edit Branch
+	$request->Patron->RegBranch			= $patron['defaultbranch']; // Patron Registration Branch
+	$request->Patron->Email				= $patron['emailaddress']; // Patron Email
 	// NON-CSV STUFF
-	$request->Patron->CollectionStatus				= 'do not send';
-	$request->Patron->EmailNotices					= 'send email';
-	$request->Patron->ExpirationDate				= date_create_from_format('Y-m-d',$patron['expirationdate'])->format('c'); // Patron Expiration Date as ISO 8601
-	$request->Patron->LastActionDate				= date('c'); // Last Action Date, format ISO 8601
-	$request->Patron->LastEditDate					= date('c'); // Patron Last Edit Date, format ISO 8601
-	$request->Patron->LastEditedBy					= 'PIK'; // Pika Patron Loader
-	$request->Patron->PatronStatusCode				= 'G'; // Patron Status Code = GOOD
-	$request->Patron->RegisteredBy					= 'PIK'; // Registered By : Pika Patron Loader
-	$request->Patron->RegistrationDate				= date('c'); // Registration Date, format ISO 8601
+	$request->Patron->CollectionStatus	= 'do not send';
+	$request->Patron->EmailNotices		= 'send email';
+	$request->Patron->ExpirationDate	= date_create_from_format('Y-m-d',$patron['expirationdate'])->format('c'); // Patron Expiration Date as ISO 8601
+	$request->Patron->LastActionDate	= date('c'); // Last Action Date, format ISO 8601
+	$request->Patron->LastEditDate		= date('c'); // Patron Last Edit Date, format ISO 8601
+	$request->Patron->LastEditedBy		= 'PIK'; // Pika Patron Loader
+	$request->Patron->PatronStatusCode	= 'G'; // Patron Status Code = GOOD
+	$request->Patron->RegisteredBy		= 'PIK'; // Registered By : Pika Patron Loader
+	$request->Patron->RegistrationDate	= date('c'); // Registration Date, format ISO 8601
 	$result = callAPI($patronApiWsdl, $requestName, $request, $tag);
 
 // SET PIN FOR CREATED PATRON
 // createPatron is not setting PIN as requested. See TLC ticket 452557
 // Therefore we use updatePatron to set PIN
 	// CREATE REQUEST
-	$requestName							= 'updatePatron';
+	$requestName						= 'updatePatron';
 	$tag								= $patron['patronid'] . ' : updatePatronPIN';
 	$request							= new stdClass();
-	$request->Modifiers						= new stdClass();
-	$request->Modifiers->DebugMode					= $patronApiDebugMode;
-	$request->Modifiers->ReportMode					= $patronApiReportMode;
-	$request->SearchType						= 'Patron ID';
-	$request->SearchID						= $patron['patronid']; // Patron ID
-	$request->Patron						= new stdClass();
+	$request->Modifiers					= new stdClass();
+	$request->Modifiers->DebugMode		= $patronApiDebugMode;
+	$request->Modifiers->ReportMode		= $patronApiReportMode;
+	$request->SearchType				= 'Patron ID';
+	$request->SearchID					= $patron['patronid']; // Patron ID
+	$request->Patron					= new stdClass();
 	if (stripos($patron['patronid'],'999') === 0) {
-		$request->Patron->PatronPIN				= '7357';
+		$request->Patron->PatronPIN		= '7357';
 	} elseif ($patron['defaultbranch'] == '7E601') {
-		$request->Patron->PatronPIN				= '1251';
+		$request->Patron->PatronPIN		= '1251';
 	} else {
-		$request->Patron->PatronPIN				= '2021';
+		$request->Patron->PatronPIN		= '2021';
 	}
 	$result = callAPI($patronApiWsdl, $requestName, $request, $tag);
 }
@@ -174,32 +173,32 @@ foreach ($all_rows as $patron) {
 	// TESTING
 	//if ($patron['patronid'] > 999115) { break; }
 	// CREATE REQUEST
-	$requestName							= 'updatePatron';
+	$requestName						= 'updatePatron';
 	$tag								= $patron['patronid'] . ' : ' . $requestName;
 	$request							= new stdClass();
-	$request->Modifiers						= new stdClass();
-	$request->Modifiers->DebugMode					= $patronApiDebugMode;
-	$request->Modifiers->ReportMode					= $patronApiReportMode;
-	$request->SearchType						= 'Patron ID';
-	$request->SearchID						= $patron['patronid']; // Patron ID
-	$request->Patron						= new stdClass();
-	$request->Patron->PatronType					= $patron['borrowertypecode']; // Patron Type
-	$request->Patron->LastName					= $patron['patronlastname']; // Patron Name Last
-	$request->Patron->FirstName					= $patron['patronfirstname']; // Patron Name First
-	$request->Patron->MiddleName					= $patron['patronmiddlename']; // Patron Name Middle
-	$request->Patron->SuffixName					= $patron['patronsuffix']; // Patron Name Suffix
-	$request->Patron->DefaultBranch					= $patron['defaultbranch']; // Patron Default Branch
-	$request->Patron->LastActionBranch				= $patron['defaultbranch']; // Patron Last Action Branch
-	$request->Patron->LastEditBranch				= $patron['defaultbranch']; // Patron Last Edit Branch
-	$request->Patron->RegBranch					= $patron['defaultbranch']; // Patron Registration Branch
+	$request->Modifiers					= new stdClass();
+	$request->Modifiers->DebugMode		= $patronApiDebugMode;
+	$request->Modifiers->ReportMode		= $patronApiReportMode;
+	$request->SearchType				= 'Patron ID';
+	$request->SearchID					= $patron['patronid']; // Patron ID
+	$request->Patron					= new stdClass();
+	$request->Patron->PatronType		= $patron['borrowertypecode']; // Patron Type
+	$request->Patron->LastName			= $patron['patronlastname']; // Patron Name Last
+	$request->Patron->FirstName			= $patron['patronfirstname']; // Patron Name First
+	$request->Patron->MiddleName		= $patron['patronmiddlename']; // Patron Name Middle
+	$request->Patron->SuffixName		= $patron['patronsuffix']; // Patron Name Suffix
+	$request->Patron->DefaultBranch		= $patron['defaultbranch']; // Patron Default Branch
+	$request->Patron->LastActionBranch	= $patron['defaultbranch']; // Patron Last Action Branch
+	$request->Patron->LastEditBranch	= $patron['defaultbranch']; // Patron Last Edit Branch
+	$request->Patron->RegBranch			= $patron['defaultbranch']; // Patron Registration Branch
 	if ($patron['collectionstatus']==0 || $patron['collectionstatus']==1 || $patron['collectionstatus']==78) {
-		$request->Patron->CollectionStatus			= 'do not send';
+		$request->Patron->CollectionStatus	= 'do not send';
 	}
-	$request->Patron->Email						= $patron['emailaddress']; // Patron Email
+	$request->Patron->Email				= $patron['emailaddress']; // Patron Email
 	if (stripos($patron['patronid'],'999') === 0) {
-		$request->Patron->PatronPIN				= '7357';
+		$request->Patron->PatronPIN		= '7357';
 	} elseif ($patron['defaultbranch'] == '7E601') {
-		$request->Patron->PatronPIN				= '1251';
+		$request->Patron->PatronPIN		= '1251';
 	}
 // PIN RESET ENDS 2020 09 01. RESTORE ON 2021 08 01
 //	elseif (stripos($patron['borrowertypecode'],'7') !== 0) {
@@ -207,11 +206,11 @@ foreach ($all_rows as $patron) {
 //	}
 	
 	// NON-CSV STUFF
-	$request->Patron->EmailNotices					= 'send email'; // Patron Email Notices
-	$request->Patron->ExpirationDate				= date_create_from_format('Y-m-d',$patron['expirationdate'])->format('c'); // Patron Expiration Date as ISO 8601
-	$request->Patron->LastActionDate				= date('c'); // Last Action Date, format ISO 8601
-	$request->Patron->LastEditDate					= date('c'); // Patron Last Edit Date, format ISO 8601
-	$request->Patron->LastEditedBy					= 'PIK'; // Pika Patron Loader
+	$request->Patron->EmailNotices		= 'send email'; // Patron Email Notices
+	$request->Patron->ExpirationDate	= date_create_from_format('Y-m-d',$patron['expirationdate'])->format('c'); // Patron Expiration Date as ISO 8601
+	$request->Patron->LastActionDate	= date('c'); // Last Action Date, format ISO 8601
+	$request->Patron->LastEditDate		= date('c'); // Patron Last Edit Date, format ISO 8601
+	$request->Patron->LastEditedBy		= 'PIK'; // Pika Patron Loader
 	$result = callAPI($patronApiWsdl, $requestName, $request, $tag);
 }
 
@@ -231,13 +230,13 @@ foreach ($all_rows as $patron) {
 	$noteIDs = explode(',', $patron['ExpiredNoteIDs']);
 	foreach ($noteIDs as $noteID) {
 		// CREATE REQUEST
-		$requestName						= 'deletePatronNote';
+		$requestName					= 'deletePatronNote';
 		$tag							= $patron['PatronID'] . ' : deleteExpiredNote ' . $noteID;
 		$request						= new stdClass();
-		$request->Modifiers					= new stdClass();
-		$request->Modifiers->DebugMode				= $patronApiDebugMode;
-		$request->Modifiers->ReportMode				= $patronApiReportMode;
-		$request->NoteID					= $noteID;
+		$request->Modifiers				= new stdClass();
+		$request->Modifiers->DebugMode	= $patronApiDebugMode;
+		$request->Modifiers->ReportMode	= $patronApiReportMode;
+		$request->NoteID				= $noteID;
 		$result = callAPI($patronApiWsdl, $requestName, $request, $tag);
 	}
 }
@@ -252,19 +251,19 @@ foreach ($iterator as $fileinfo) {
         $mtime = $fileinfo->getMTime();
 	$matches = [];
         if ($fileinfo->isFile() && preg_match('/^(\d{6,7}).jpg$/', $file, $matches) === 1 && $mtime >= $today) {
-		$requestName						= 'updateImage';
+		$requestName					= 'updateImage';
 		$tag							= $matches[1] . ' : ' . $requestName;
 		$request						= new stdClass();
-		$request->Modifiers					= new stdClass();
-		$request->Modifiers->DebugMode				= $patronApiDebugMode;
-		$request->Modifiers->ReportMode				= $patronApiReportMode;
-		$request->SearchType					= 'Patron ID';
-		$request->SearchID					= $matches[1]; // Patron ID
-		$request->ImageType					= 'Profile'; // Patron Profile Picture vs. Signature
-		$imageFilePath 						= "../data/images/staff/" . $file;
+		$request->Modifiers				= new stdClass();
+		$request->Modifiers->DebugMode	= $patronApiDebugMode;
+		$request->Modifiers->ReportMode	= $patronApiReportMode;
+		$request->SearchType			= 'Patron ID';
+		$request->SearchID				= $matches[1]; // Patron ID
+		$request->ImageType				= 'Profile'; // Patron Profile Picture vs. Signature
+		$imageFilePath 					= "../data/images/staff/" . $file;
 		if (file_exists($imageFilePath)) {
-			$imageFileHandle 				= fopen($imageFilePath, "rb");
-			$request->ImageData				= fread($imageFileHandle, filesize($imageFilePath));
+			$imageFileHandle 			= fopen($imageFilePath, "rb");
+			$request->ImageData			= fread($imageFileHandle, filesize($imageFilePath));
 			fclose($imageFileHandle);
 		} else {
 // TO DO: create IMAGE NOT AVAILABLE image
