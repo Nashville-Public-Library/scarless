af393991782fa31f3746806612996b53015b4d6c
James Staub <james.staub@nashville.gov>
2024-09-22 14:34:47 -0500

    Image report: include a tally of missing images

--- ic2carlx_mnps_students_image_report.sh
+++ ic2carlx_mnps_students_image_report.sh
@@ -8,35 +8,67 @@
 # Define the paths
 image_dir="../data/images/students/"
 csv_file="../data/ic2carlx_mnps_students_infinitecampus.csv"
-derivative_csv_file="../data/ic2carlx_mnps_students_infinitecampus_derivative.csv"
-output_file="../data/output.txt"
-find_file="../data/ic2carlx_mnps_students_infinitecampus_images_old.txt"
-join_file="../data/ic2carlx_mnps_students_infinitecampus_images_old_join.txt"
+derivative_id_branch_csv_file="../data/ic2carlx_mnps_students_infinitecampus_derivative.csv"
+find_images_file="../data/ic2carlx_mnps_students_images_exist.txt"
+find_images_missing_file="../data/ic2carlx_mnps_students_infinitecampus_images_missing_join.txt"
+find_old_images_file="../data/ic2carlx_mnps_students_images_old.txt"
+find_old_images_join_file="../data/ic2carlx_mnps_students_infinitecampus_images_old_join.txt"
+output_missing_images_file="../data/ic2carlx_mnps_students_images_missing_summary.txt"
+output_old_images_file="../data/output.txt"
+combined_output_file="../data/ic2carlx_mnps_students_images_summary.txt"
 
-# Clear the output file
-> "$output_file"
+# Clear all output files
+> "$find_images_file"
+> "$find_images_missing_file"
+> "$find_old_images_file"
+> "$find_old_images_join_file"
+> "$output_missing_images_file"
+> "$output_old_images_file"
+> "$combined_output_file"
 
 # Define the cutoff date
 cutoff_date="2022-08-01"
 
-# Create the derivative CSV file with only the necessary columns
-echo "Creating derivative CSV file..."
-awk -vFPAT='[^,]*|"[^"]*"' -F, '{print $1 "," $12}' "$csv_file" > "$derivative_csv_file"
-sort -t, -k1,1 "$derivative_csv_file" -o "$derivative_csv_file"
+# Create the derivative CSV file with only the patron id and branch code
+echo "Creating derivative CSV file from $csv_file "
+awk -vFPAT='[^,]*|"[^"]*"' -F, '$1 < 190999000 {print $1 "," $12}' "$csv_file" > "$derivative_id_branch_csv_file"
+sort -t, -k1,1 "$derivative_id_branch_csv_file" -o "$derivative_id_branch_csv_file"
 
-# Output the find command results to a temporary file
+# Output the find images command results to a file
+echo "Finding all images..."
+find "$image_dir" -type f -printf "%f\n" | grep -oP '\d{9}' > "$find_images_file"
+sort "$find_images_file" -o "$find_images_file"
+
+# Join the find results with the derivative CSV file
+echo "Creating the list of missing files by branch codes..."
+join -t, -1 1 -2 1 -v 2 "$find_images_file" "$derivative_id_branch_csv_file" > "$find_images_missing_file"
+
+# Create the summary output of missing images by branch code
+echo "Creating summary output of missing images by branch code..."
+awk -F, '{count[$2]++} END {for (branch in count) print branch, count[branch]}' "$find_images_missing_file" > "$output_missing_images_file"
+sort "$output_missing_images_file" -o "$output_missing_images_file"
+echo "Summary output created. Output written to $output_missing_images_file"
+# Display the summary output
+cat "$output_missing_images_file"
+
+# Output the find-old-images command results to a file
 echo "Finding files older than $cutoff_date..."
-find "$image_dir" -type f ! -newermt "$cutoff_date" -printf "%f\n" | grep -oP '\d{9}' > "$find_file"
-sort "$find_file" -o "$find_file"
+find "$image_dir" -type f ! -newermt "$cutoff_date" -printf "%f\n" | grep -oP '\d{9}' > "$find_old_images_file"
+sort "$find_old_images_file" -o "$find_old_images_file"
 
 # Join the find results with the derivative CSV file
 echo "Creating the list of too-old files and branch codes..."
-join -t, -1 1 -2 1 -o 1.1,2.2 "$find_file" "$derivative_csv_file" > "$join_file"
+join -t, -1 1 -2 1 -o 1.1,2.2 "$find_old_images_file" "$derivative_id_branch_csv_file" > "$find_old_images_join_file"
 
-# Create the summary output
+# Create the summary output of old images by branch code
 echo "Creating summary output..."
-awk -F, '{count[$2]++} END {for (branch in count) print branch, count[branch]}' "$join_file" > "$output_file"
-sort "$output_file" -o "$output_file"
-echo "Summary output created. Output written to $output_file"
+awk -F, '{count[$2]++} END {for (branch in count) print branch, count[branch]}' "$find_old_images_join_file" > "$output_old_images_file"
+sort "$output_old_images_file" -o "$output_old_images_file"
+echo "Summary output created. Output written to $output_old_images_file"
 # Display the summary output
-cat "$output_file"
+cat "$output_old_images_file"
+
+# Combine the files on the id column
+join -t, -1 1 -2 1 "$output_missing_images_file" "$output_old_images_file" > "$combined_output_file"
+# Display the combined output
+cat "$combined_output_file"
