807971fa3dace042858e5a2b7a3bbe6829b1d8e0
James Staub <james.staub@nashville.gov>
2024-09-22 12:27:26 -0500

    Image report: cutoff date should be 2022

--- ic2carlx_mnps_students_image_report.sh
+++ ic2carlx_mnps_students_image_report.sh
@@ -12,7 +12,7 @@ join_file="../data/ic2carlx_mnps_students_infinitecampus_images_old_join.txt"
 > "$output_file"
 
 # Define the cutoff date
-cutoff_date="2024-08-01"
+cutoff_date="2022-08-01"
 
 # Create the derivative CSV file with only the necessary columns
 echo "Creating derivative CSV file..."
@@ -22,7 +22,7 @@ sort -t, -k1,1 "$derivative_csv_file" -o "$derivative_csv_file"
 echo "Finding files older than $cutoff_date..."
 find "$image_dir" -type f ! -newermt "$cutoff_date" -printf "%f\n" | grep -oP '\d{9}' | sort > "$find_file"
 # Join the find results with the derivative CSV file
-echo "Creating the list of too-old files and branch codes"
+echo "Creating the list of too-old files and branch codes..."
 join -t, -1 1 -2 1 -o 1.1,2.2 -v 1 "$find_file" "$derivative_csv_file" > "$join_file"
 # Create the summary output
 echo "Creating summary output..."
