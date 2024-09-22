1f91c0fc26c8aa08823664ede985c28c42d21e70
James Staub <james.staub@nashville.gov>
2024-09-22 12:38:21 -0500

    Image report: join is not working, attempt new sort

--- ic2carlx_mnps_students_image_report.sh
+++ ic2carlx_mnps_students_image_report.sh
@@ -18,12 +18,16 @@ cutoff_date="2022-08-01"
 echo "Creating derivative CSV file..."
 awk -vFPAT='[^,]*|"[^"]*"' -F, '{print $1 "," $12}' "$csv_file" > "$derivative_csv_file"
 sort -t, -k1,1 "$derivative_csv_file" -o "$derivative_csv_file"
+
 # Output the find command results to a temporary file
 echo "Finding files older than $cutoff_date..."
-find "$image_dir" -type f ! -newermt "$cutoff_date" -printf "%f\n" | grep -oP '\d{9}' | sort > "$find_file"
+find "$image_dir" -type f ! -newermt "$cutoff_date" -printf "%f\n" | grep -oP '\d{9}' > "$find_file"
+sort "$find_file" -o "$find_file"
+
 # Join the find results with the derivative CSV file
 echo "Creating the list of too-old files and branch codes..."
 join -t, -1 1 -2 1 -o 1.1,2.2 -v 1 "$find_file" "$derivative_csv_file" > "$join_file"
+
 # Create the summary output
 echo "Creating summary output..."
 awk -F, '{count[$2]++} END {for (branch in count) print branch, count[branch]}' "$join_file" > "$output_file"
