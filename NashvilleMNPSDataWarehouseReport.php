313fcd21140ca4149ef0c56741f4b2c92725b0ec
James Staub <james.staub@nashville.gov>
2024-12-07 20:02:14 -0600

    Improve data warehouse report
    https://trello.com/c/eViB7ddl
    + switch from envbranch to transaction patron branch of registration as the basis for schoolCode
    + exclude test patron accounts
    + ensure LEAD Academy student checkouts are recorded as LEAD Academy and not LEAD Cameron
    + ensure when scarless fails to update student school the checkout school is updated in the report
    + delete unassignable records

--- NashvilleMNPSDataWarehouseReport.php
+++ NashvilleMNPSDataWarehouseReport.php
@@ -55,31 +55,82 @@ class nashvilleMNPSDataWarehouseReport
 
 		$sql = <<<EOT
 -- MNPS SCHOOL LIBRARY MONTHLY CHECKOUTS
--- Does NOT include Limitless Libraries
--- Does include checkouts to MNPS staff
+-- Does NOT include Limitless Libraries envbranch checkouts
+-- Does NOT include NPL Item checkouts
+-- Does NOT include checkouts to MNPS staff
+-- Does NOT include transactions where checkout is likely assigned to the wrong patron
 with co as (
     select
         *
     from txlog_v2
-    where systemtimestamp >= add_months(trunc(sysdate, 'MON'), -$monthsDifference) -- provided month
-    and systemtimestamp < add_months(trunc(sysdate, 'MON'), -$monthsDifference + 1) -- next month after provided month
+    where systemtimestamp >= add_months(trunc(sysdate, 'MON'), -1) -- last full month
+    and systemtimestamp < add_months(trunc(sysdate, 'MON'), -0) -- last full month
     and transactiontype = 'CH'
 )
 , cos as (
     select
-        *
+        substr(bre.branchcode,-3) as schoolcode_e
+        , substr(bri.branchcode,-3) as schoolcode_i
+        , substr(brp.branchcode,-3) as schoolcode_p
+        , to_char(co.systemtimestamp, 'YYYY-MM-DD') as checkoutdate
+--        , to_char(co.systemtimestamp, 'HH24:MI:SS') as checkouttime -- useful for troubleshooting
+        , co.*
     from co
-    left join branch_v2 br on co.envbranch = br.branchnumber
-    where co.envbranch != 29 -- Exclude the Limitless Libraries checkout terminals
-    and branchgroup = 2
+    left join branch_v2 bre on co.envbranch = bre.branchnumber
+    left join branch_v2 bri on co.itembranch = bri.branchnumber
+    left join branch_v2 brp on co.patronbranchofregistration = brp.branchnumber
+    where regexp_like(patronid, '^190[0-9]{6}$') -- MNPS student IDs start with 190, followed by 6 digits, i.e., NOT MNPS staff or NPL patrons
+    and patronid not like '190999%' -- Exclude test student patronids
+    and co.envbranch != 29 -- Exclude the Limitless Libraries checkout terminals
+    and bre.branchgroup = 2 -- Only envbranch at MNPS
+    and bri.branchgroup = 2 -- Only MNPS items
 )
+--select * from cos; -- useful for troubleshooting when you want transactions
 , coss as (
     select
-        substr(cos.branchcode, -3) as schoolCode
+--
+-- CASE STATEMENT BELOW TO BE USED ONLY IF SCHOOLCODE IS BASED ON ENVBRANCH
+-- HANDLES LEAD CAMERON COLLEGE PREP -> LEAD ACADEMY HIGH
+--        case
+--            when schoolcode_e = '71181' and schoolcode_p = '78508' then '508' -- LEAD Cameron envbranch + LEAD Academy patronbranch = LEAD Academy High School -- it might be useful to include 'and patronbty in (31,32,33,34,37,46,47)'...
+--            else substr(cos.branchcode, -3) 
+--        end as schoolCode
+--
+-- CASE STATEMENT BELOW TO BE USED ONLY IF SCHOOLCODE IS BASED ON PATRON
+-- HANDLES GLITCHES IN SCARLESS WHERE STUDENT SCHOOL FAILS TO UPDATE
+-- I.E., WHEN ENV BRANCH MATCHES ITEM BRANCH, BUT PATRON BRANCH DOES NOT MATCH 
+-- AND THE RUNTIME PATRON BRANCH DOES MATCH ENV AND ITEM, 
+-- THEN CHANGE TRANSACTION PATRON BRANCH TO THE RUNTIME PATRON BRANCH
+        case
+        -- LEAD Cameron/LEAD Academy: 181 = env = item, but patron = 508 OR runtime patron = 508
+            when schoolcode_e = '181'
+                and schoolcode_i = '181'
+                and (schoolcode_p = '508' or substr(b.branchcode,-3) = '508')
+                then schoolcode_p -- i.e., 508
+        -- env = item = runtime patron defaultbranch != tx patronregistrationbranch; assume scarless failed to update student location, make schoolcode = env branch
+            when schoolcode_e = schoolcode_i 
+                and schoolcode_e != schoolcode_p 
+                and schoolcode_e = substr(b.branchcode,-3) 
+                then schoolcode_e
+        -- env = item != runtime patron defaultbranch != tx patronregistrationbranch; assume checked out item assigned to wrong patron, make schoolcode NULL
+            when  schoolcode_e = schoolcode_i 
+                and schoolcode_e != schoolcode_p 
+                and schoolcode_e != substr(b.branchcode,-3)
+                then NULL
+        -- env != item != patron; assume THE ENDTIMES ARE UPON US... RUN!!! and make schoolCode NULL
+            when schoolcode_e != schoolcode_i
+                and schoolcode_e != schoolcode_p
+                and schoolcode_i != schoolcode_p
+                then NULL
+        -- item = patron != env (e.g., librarian logs in as the wrong branch)
+        -- OR env = patron != item (e.g., librarian has snuck into another school and stolen books and not bothered to change owningbranch in Carl)
+            else substr(cos.schoolcode_p, -3) 
+        end as schoolCode
         , to_char(cos.systemtimestamp, 'YYYYMMDD') as yearMonthDay
-        , patronid as studentID
-    from cos
-    where regexp_like(patronid, '^190[0-9]{6}$') -- MNPS student IDs start with 190, followed by 6 digits
+        , cos.patronid as studentID
+    from cos 
+    left join patron_v2 p on cos.patronid = p.patronid
+    left join branch_v2 b on p.defaultbranch = b.branchnumber
 )
 select
     schoolcode
@@ -87,7 +138,9 @@ select
     , studentid
     , count(*) as countOfCheckouts
 from coss
+where schoolCode is not null -- skip records where schoolCode is NULL
 group by schoolcode, yearmonthday, studentid
+order by studentid, yearmonthday, schoolcode
 EOT;
 		// connect to carlx oracle db
 		$conn = oci_connect($this->carlx_db_php_user, $this->carlx_db_php_password, $this->carlx_db_php, 'AL32UTF8');
