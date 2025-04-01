9f5f215076d12b76f7a9ba74b80f638da963876f
James Staub <james.staub@nashville.gov>
2025-04-01 16:53:03 -0500

    Mackin report continued

--- NashvilleMNPSDataWarehouseReport-Mackin.sh
+++ NashvilleMNPSDataWarehouseReport-Mackin.sh
@@ -2,13 +2,9 @@
 # NashvilleMNPSDataWarehouseReport.sh
 # James Staub, Nashville Public Library
 
-#!/bin/bash
-# NashvilleMNPSDataWarehouseReport.sh
-# James Staub, Nashville Public Library
-
 # Read the configuration file
-mackinUser=$(awk -F "=" '/mackinUser/ {print $2}' ../config.pwd.ini | tr -d ' ')
-mackinPassword=$(awk -F "=" '/mackinPassword/ {print $2}' ../config.pwd.ini | tr -d ' ')
+mackinUser=$(awk -F "=" '/mackinUser/ {print $2}' ../config.pwd.ini | tr -d ' ' | sed 's/^"\(.*\)"$/\1/')
+mackinPassword=$(awk -F "=" '/mackinPassword/ {print $2}' ../config.pwd.ini | tr -d ' ' | sed 's/^"\(.*\)"$/\1/')
 
 # Determine the date of the report record activity (either from arg1 or yesterday)
 if [ $# -gt 0 ]; then
@@ -27,18 +23,25 @@ date_mmddyyyy=$(date -d "$date" +'%m_%d_%Y')
 # set date for output filename
 date_yyyymmdd=$(date -d "$date" +'%Y-%m-%d')
 
-# Retrieve the report files from Mackin
+# Retrieve the STAFF AND STUDENT report files from Mackin
 sshpass -p "$mackinPassword" sftp "$mackinUser"@sftp.mackin.com:Reports/*"$date_mmddyyyy"* ../data/mackin/
 
-# Read the SQL file
-sql=$(<NashvilleMNPSDataWarehouseReport-Mackin.sql)
-
-# Replace placeholders with the actual date
+# STAFF: Read the SQL file
+sql=$(<NashvilleMNPSDataWarehouseReport-Mackin-Staff.sql)
+# STAFF: Replace placeholders with the actual date
 sql=${sql//DATEPLACEHOLDERMMDDYYYY/$date_mmddyyyy}
 sql=${sql//DATEPLACEHOLDERYYYYMMDD/$date_yyyymmdd}
+# STAFF: Write the modified SQL - incorporating custom date - to a new file
+echo "$sql" > NashvilleMNPSDataWarehouseReport-Mackin-Staff-Date-Specific.sql
+# STAFF: Run the modified SQL file through sqlite3
+sqlite3 ../data/ic2carlx_mnps_staff.db < NashvilleMNPSDataWarehouseReport-Mackin-Staff-Date-Specific.sql
 
-# Write the modified SQL - incorporating custom date - to a new file
-echo "$sql" > NashvilleMNPSDataWarehouseReport-Mackin-Date-Specific.sql
-
-# Run the modified SQL file through sqlite3
-sqlite3 ../data/ic2carlx_mnps_staff.db < NashvilleMNPSDataWarehouseReport-Mackin-Date-Specific.sqlmnps_staff.db < NashvilleMNPSDataWarehouseReport-Mackin-Modified.sql")
+# STUDENTS: Read the SQL file
+sql=$(<NashvilleMNPSDataWarehouseReport-Mackin-Students.sql)
+# STUDENTS: Replace placeholders with the actual date
+sql=${sql//DATEPLACEHOLDERMMDDYYYY/$date_mmddyyyy}
+sql=${sql//DATEPLACEHOLDERYYYYMMDD/$date_yyyymmdd}
+# STUDENTS: Write the modified SQL - incorporating custom date - to a new file
+echo "$sql" > NashvilleMNPSDataWarehouseReport-Mackin-Students-Date-Specific.sql
+# STUDENTS: Run the modified SQL file through sqlite3
+sqlite3 ../data/ic2carlx_mnps_students.db < NashvilleMNPSDataWarehouseReport-Mackin-Students-Date-Specific.sql
