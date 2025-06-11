176c46712fa4cedb85315587a37e662d0dcca952
James Staub <james.staub@nashville.gov>
2025-06-10 20:09:38 -0500

    Add script that inserts a column into the exceptions file

---
+++ insert_column_into_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# insert_column_into_test.sh
+# James Staub, Nashville Public Library
+# Script to insert a new column into ic2carlx_mnps_students_test.txt based on unique default branch values from CARLX_INFINITECAMPUS_STUDENT.txt
+
+# Get unique default branch values from CARLX_INFINITECAMPUS_STUDENT.txt
+# to determine which schools actually have promising scholars students
+unique_branches=$(awk -F '|' '{print $20}' ../data/CARLX_INFINITECAMPUS_STUDENT.txt | sort | uniq)
+
+# Insert the new column into the original file
+for row in *; do
+  IFS='|' read -r -a values <<< "$row"
+  default_branch=${values[17]}
+
+  # If the 18th column value matches a unique branch value, set the new column value to it
+  if [[ $unique_branches =~ $default_branch ]]; then
+    sed -i "s/|/$default_branch|/" ic2carlx_mnps_students_test.txt
+  else
+    sed -i "/$row/s/[^|]*$|/$row|promisingScholarsBranch/|/" ic2carlx_mnps_students_test.txt
+  fi
+done
