95079e9899153ad22187dd790082aae7988a1f36
James Staub <james.staub@nashville.gov>
2025-06-10 21:36:05 -0500

    Update insert_column_into_test.sh
    debug

--- insert_column_into_test.sh
+++ insert_column_into_test.sh
@@ -9,13 +9,16 @@ output_file="../data/ic2carlx_mnps_students_test_with_branch.txt"
 awk -F'|' -v OFS='|' -v branches="{$unique_branches}" '
   BEGIN {
     n = split(branches, arr, ",");
+    print "DEBUG: Populating b with these keys:";
     for (i = 1; i <= n; i++) {
       gsub(/"/, "", arr[i]);
       b[arr[i]] = 1;
+      print "  b[" arr[i] "] = 1";
     }
   }
   {
-    newcol = (b[$19]) ? $19 : "";
+    print "DEBUG: Row " NR ": $19=" $19 ", $18=" $18 ", b[$18]=" (b[$18] ? "1" : "0");
+    newcol = (b[$18]) ? $19 : "";
     for (i = 1; i <= 19; i++) printf "%s%s", $i, OFS;
     printf "%s", newcol;
     for (i = 20; i <= NF; i++) printf "%s%s", OFS, $i;
