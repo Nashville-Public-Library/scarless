b7b848810f3c547709a8c1d294a21eb41f358ab8
James Staub <james.staub@nashville.gov>
2026-07-15 08:07:07 -0500

    CollectionCountCarlX: limit and correct report date to today

--- NashvilleMNPSDataWarehouseReport-CollectionCountCarlX.sh
+++ NashvilleMNPSDataWarehouseReport-CollectionCountCarlX.sh
@@ -2,14 +2,7 @@
 # NashvilleMNPSDataWarehouseReport-CollectionCountCarlX.sh
 # This script runs the PHP script NashvilleMNPSDataWarehouseReport-CollectionCountCarlX.php and then moves the files to the destination directory.
 
-# Check the number of arguments
-if [ "$#" -eq 0 ]; then
-  # No arguments: run the PHP script without arguments
-  php NashvilleMNPSDataWarehouseReport-CollectionCountCarlX.php
-else
-  # Pass the argument to the PHP script (e.g., a specific date)
-  php NashvilleMNPSDataWarehouseReport-CollectionCountCarlX.php "$1"
-fi
+php NashvilleMNPSDataWarehouseReport-CollectionCountCarlX.php
 
 # Move the file to the destination directory
 SOURCE_FILE="../data/LibraryServices-CollectionCountCarlX-MNPS-*"
